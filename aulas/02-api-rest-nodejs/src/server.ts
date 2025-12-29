3b01471b9e4a3f786fb3f7a7216cd979ab1befbf
nathansouza7 <nathansouza787@gmail.com>
2025-12-28 23:03:58 -0300

    handling env with zod

--- aulas/02-api-rest-nodejs/src/server.ts
+++ aulas/02-api-rest-nodejs/src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify"
 import crypto from 'node:crypto'
 import { knex } from "./database.js"
+import { env } from "./env/index.js"
 
 const app = fastify()
 
@@ -12,7 +13,7 @@ app.get("/hello", async () => {
 })
 
 app.listen({
-    port: 3333,
+    port: env.PORT,
 }).then(() => {
     console.log("HTTP Server Running!")
 })
