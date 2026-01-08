7edcc8dd277d26701ec543c73cd715c1f839dd25
nathansouza7 <nathansouza787@gmail.com>
2026-01-08 16:54:55 -0300

    create transaction

--- aulas/02-api-rest-nodejs/src/server.ts
+++ aulas/02-api-rest-nodejs/src/server.ts
@@ -2,14 +2,12 @@ import fastify from "fastify"
 import crypto from 'node:crypto'
 import { knex } from "./database.js"
 import { env } from "./env/index.js"
+import { transactionsRoutes } from "./transactions.js"
 
 const app = fastify()
 
-app.get("/hello", async () => {
-  const transactions = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-  return transactions
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app.listen({
